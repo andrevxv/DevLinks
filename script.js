b233ba8caf4017831d0b8f541e35e499f9030653
André Vale <devvvale@gmail.com>
2023-07-11 22:40:27 -0300

    set links and descriptions

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function toggleMode() {
   // substituir a imagem se
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar.png")
   } else {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
